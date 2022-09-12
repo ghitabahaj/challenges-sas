ddcb9915ac65cecbe10c2e15e1a1d68f5d65f5cb
bahaj <bahajghita3@gmail.com>
2022-09-12 14:18:26 +0100

    update

--- semaine2/challenge 5.c
+++ semaine2/challenge 5.c
@@ -5,7 +5,26 @@
 
 int main(){
 	
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+
+int nbRinvers=0, nbr;
+
+ printf("entree nomber : ");
+ scanf("%d", &nbr);
+
+ while (nbr != 0)
+ {
+   nbRinvers = nbRinvers * 10;
+   nbRinvers = nbRinvers + nbr % 10;
+   nbr = nbr / 10;
+ }
+
+ printf("nomber invers est : %d", nbRinvers);
+
+}
 	
 	return 0;
 }
