8e74f407fd175d4a6875d91846f2b4a1593b6de0
bahaj <bahajghita3@gmail.com>
2022-09-13 11:54:01 +0100

    update challenge

--- semaine2/challenge4.c
+++ semaine2/challenge4.c
@@ -4,20 +4,37 @@
 
 int main ()
 {
-int somme=0 , max = 0 , n;
+int somme=0 , max = 0 , n =1;
+	
+while (n>0 && n<100){
+	 printf ("entrez un nombre :");
+     scanf("%d",&n);
+     
+     if (n==0){break;}
+     if (n>100){continue;}
+     
+     somme = somme + n;
+     if(max<n){
+     	max = n;
+	 }
+}
 	
 	
-  for(int i=1;i<10;i++){
-  	printf ("entrez un nombre :");
-	scanf("%d",&n);
-	printf("\n");
-  	if(n%10==0 && n<100){
-  		somme = somme + n;
-  		if(max < n){
-  	max = n;
-  }
-  } 
-  }
+
+	
+//  for(int i=1;i<10;i++){
+//  	printf ("entrez un nombre :");
+//	scanf("%d",&n);
+//	printf("\n");
+//  	if(n<100){
+//  		somme = somme + n;
+//  		if(max < n){
+//  	max = n;
+//  }
+//  }else if(n==0){
+//  	   break;
+//  } 
+//  }
   
   printf(" la somme de ces nombres est : %d \n", somme);
   printf("le max de ces nombres est : %d", max);
