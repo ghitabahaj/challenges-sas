7443035c4daf6bf26193b8c896dbfc1784cf607d
bahaj <bahajghita3@gmail.com>
2022-09-13 17:05:50 +0100

    updateeeeeeeeeee

--- semaine2/challenge 9.c
+++ semaine2/challenge 9.c
@@ -7,21 +7,25 @@ int main(){
 	
 int n= 0 , p = 0 , res = 1;
 
-printf("entrez un nombre svp:");
-scanf("%d",&n);
-printf("\n");
-printf("entrez la puissance svp:");
-scanf("%d",&p);
- 
+do{
+	printf("entrez un nombre superieure strictement a 0  svp:");
+	scanf("%d",&n);
+}while(n<=0);
+
+do{
+	printf("\n entrez une puissance positif svp!!!!!!:");
+	scanf("%d",&p);
+}while(p<0);
 int d = p;
 
-while(n>0 && p>0){
-	
-	    res = res * n ;
+while(n>0 && p>=0){
+	if(p==0){
+		res =  res * 1;
 		p--;
-	
+}else{
+	    res = res * n ;
+		p--;}
 }
-
 printf(" %d a la puissance %d egal a : %d", n , d , res);
 
 
